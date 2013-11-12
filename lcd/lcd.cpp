ecae9ec173ce3acc8f71b178245462d06df132db
Toby <tobyc@dryft.net>
2013-11-13 01:33:53 +1100

    Simple test of jhd 162a lcd board

---
+++ lcd/lcd.cpp
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+#include "LiquidCrystal.h"
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+    lcd.begin(16,2);
+    lcd.print("hello, world");
+}
+
+void loop() {
+    lcd.setCursor(0, 1);
+    lcd.print(millis()/1000);
+}
