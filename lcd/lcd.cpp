1a59b0b26f4f9e97e2e9cb83fd9c83f3c119e47a
Toby <tobyc@dryft.net>
2013-11-18 00:23:59 +1100

    current state of play of LCD driver; also clicks

--- lcd/lcd.cpp
+++ lcd/lcd.cpp
@@ -5,10 +5,39 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
     lcd.begin(16,2);
-    lcd.print("hello, world");
+    pinMode(8, OUTPUT);
+    digitalWrite(8, LOW);
 }
 
 void loop() {
+    int alc;
+    int perc; 
+
+    lcd.setCursor(0, 0);
+    lcd.print("Radiation  level");
+
+    alc = analogRead(5);
+    perc = float(alc) / float(102400);
+
     lcd.setCursor(0, 1);
-    lcd.print(millis()/1000);
+    lcd.print(perc, DEC);
+    lcd.print("% (");
+    lcd.print(alc, DEC);
+    lcd.print(")");
+   
+    tone(8, 4000, 5);
+    //digitalWrite(8, HIGH);
+    //delay(5);
+    //digitalWrite(8, LOW);
+    delay(1250 - alc);
+
+}
+
+void flash_danger() {
+    lcd.setCursor(0, 1);
+    lcd.print("-=== DANGER ===-");
+    delay(1000);
+    lcd.setCursor(0, 1);
+    lcd.print("-===        ===-");
+    delay(1000);
 }
