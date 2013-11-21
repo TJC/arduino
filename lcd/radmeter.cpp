258a80d4509e8bcc4b3ed4d5833cfba0aa867681
Toby <tobyc@dryft.net>
2013-11-21 23:24:49 +1100

    tweak green LEDs to be a bit dimmer. Were way too bright!

--- lcd/radmeter.cpp
+++ lcd/radmeter.cpp
@@ -137,15 +137,15 @@ void clicker(uint16_t val) {
     last_click = cur_millis + cdelay; // adds some random timing
 }
 
-// Actually more like a mangled, offset sine..
-inline static short val_to_led_scale_128(uint16_t val) {
+// Actually more like a mangled, offset half-sine..
+inline static short val_to_led_scale(uint16_t val, short scale) {
     if (val < 256) {
         return 0;
     }
     else if (val > 512) {
-        return 128;
+        return scale;
     }
-    return int(128.0 * sinf(1.5707 * float(val-256)/256.0));
+    return int(float(scale) * sinf(1.5707 * float(val-256)/256.0));
 }
 
 void cycle_leds(uint16_t val) {
@@ -165,11 +165,12 @@ void cycle_leds(uint16_t val) {
     if (cur_millis - elapsed < delayer) { return; }
     elapsed = cur_millis;
 
-    red = val_to_led_scale_128(val) - colour_adj;
-    green = val_to_led_scale_128(1023-val) - colour_adj;
+    red = val_to_led_scale(val, 128) - colour_adj;
+    // green is very bright!
+    green = val_to_led_scale(1023-val, 96) - colour_adj;
 
     // cap values at moderately-high brightness:
-    if (green > 128) { green = 129; }
+    if (green > 96) { green = 97; }
     if (red > 128) { red = 129; }
     // cap underflows
     if (red < 0) { red = 0; }
