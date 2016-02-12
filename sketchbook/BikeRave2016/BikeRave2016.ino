8f844760ba7f0d411f4143f8c46afdb164d6e6b2
Toby Corkindale <tjc@wintrmute.net>
2016-02-12 17:09:15 +1100

    tweak LED displays

--- sketchbook/BikeRave2016/BikeRave2016.ino
+++ sketchbook/BikeRave2016/BikeRave2016.ino
@@ -5,6 +5,7 @@ FASTLED_USING_NAMESPACE
 #define DATA_PIN 8
 #define NUM_LEDS    60
 CRGB leds[NUM_LEDS];
+bool midCopLights = false;
 
 #define BRIGHTNESS          96
 #define FRAMES_PER_SECOND  120
@@ -19,26 +20,26 @@ void addGlitter( fract8 chanceOfGlitter);
 void confetti();
 void sinelon();
 void bpm();
+void bpm2();
 void juggle();
 void copLights();
 
 void setup() {
-  delay(3000); // 3 second delay for recovery
+  delay(2000); // 2 second delay for recovery
   
   // tell FastLED about the LED strip configuration
-  //FastLED.addLeds<LED_TYPE,DATA_PIN>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
 
-  copLights();
+  randomSeed(analogRead(0));
 }
 
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { copLights, confetti, sinelon, juggle, bpm };
+SimplePatternList gPatterns = { bpm2, copLights, confetti, bpm, rainbowWithGlitter, juggle };
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
@@ -54,8 +55,8 @@ void loop()
   FastLED.delay(1000/FRAMES_PER_SECOND); 
 
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 10 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  EVERY_N_MILLISECONDS( 5 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_SECONDS( 30 ) { nextPattern(); } // change patterns periodically
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
@@ -66,29 +67,47 @@ void nextPattern()
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
 }
 
-void fill(CRGB col) {
-    for (int i=0; i<NUM_LEDS; i++) {
-      leds[i] = col;
-    }
-    FastLED.show();  
+inline void fill(CRGB col) {
+  fill_solid(leds, NUM_LEDS, col);
+  FastLED.show();  
 }
+
+CRGB primary_colours[] = {
+  CRGB::Blue,
+  CRGB::Blue,
+  CRGB::Red,
+  CRGB::Red,
+  CRGB::Green,
+  CRGB::Yellow,
+  CRGB::Purple,
+  CRGB::Black
+};
+#define num_pri_colours 8
 
 void copLights()
 {
-  for (int i=0; i<20; i++) {
-    for (int j=0; j<3; j++) {
-      fill(CRGB::Blue);
-      delay(50);
-      fill(CRGB::Black);
-      delay(50);
+  static int col1, col2;
+  if (! midCopLights) {
+    col2 = random8(num_pri_colours);
+    col1 = random8(num_pri_colours);
+    while (col2 == col1) {
+      col2 = random8(num_pri_colours);
     }
+    midCopLights = true;
+  }
 
-    for (int j=0; j<3; j++) {
-      fill(CRGB::Red);
-      delay(50);
-      fill(CRGB::Black);
-      delay(50);
-    }
+  for (int j=0; j<4; j++) {
+    fill(primary_colours[col1]);
+    delay(60);
+    fill(CRGB::Black);
+    delay(60);
+  }
+
+  for (int j=0; j<4; j++) {
+    fill(primary_colours[col2]);
+    delay(60);
+    fill(CRGB::Black);
+    delay(60);
   }
 }
 
@@ -114,6 +133,9 @@ void addGlitter( fract8 chanceOfGlitter)
 
 void confetti() 
 {
+  // XXX hack, reset double blink effect
+  if (midCopLights) { midCopLights = false; }
+
   // random colored speckles that blink in and fade smoothly
   fadeToBlackBy( leds, NUM_LEDS, 10);
   int pos = random16(NUM_LEDS);
@@ -131,12 +153,22 @@ void sinelon()
 void bpm()
 {
   // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  uint8_t BeatsPerMinute = 130;
+  CRGBPalette16 palette = HeatColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 32, 240);
   for( int i = 0; i < NUM_LEDS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+    leds[i] = ColorFromPalette(palette, (i*2), beat+(i*16));
   }
+}
+
+void bpm2()
+{
+  uint8_t BeatsPerMinute = 130;
+  CRGBPalette16 palette = HeatColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 0, 160);
+  fill_solid(leds, NUM_LEDS,
+    ColorFromPalette(palette, beat, 255)
+  );
 }
 
 void juggle() {
