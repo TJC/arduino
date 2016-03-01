9d83929b4a94d180c15ece4d71948d5b8211711d
Toby <tjc@wintrmute.net>
2016-03-01 15:39:59 +1100

    looks like I had some uncommitted changes on this laptop

--- sketchbook/anglerfish/anglerfish.ino
+++ sketchbook/anglerfish/anglerfish.ino
@@ -1,20 +1,30 @@
 #include "Arduino.h"
 #include <stdlib.h>
 #include <FastLED.h>
-// Was built against version 3.1
+// Was built against version 3.1.0
+
+/*
+ * Anglerfish LED code. Assumes use of OctoWS2811 PortD pins via adapter board.
+ * Was thrown together quickly due to rather short amounts of time
+ * to spend working on this; would love to have had more time.
+ * One extra effect I have planned is "bubbles" travelling upwards along
+ * vertically mounted LED strips.
+* Longest strip is 3950mm = 118 or 119 LEDs
+ * - Toby
+ */
 
 // Notes:
 // HSV Blue is ~140 to 180 (going from cyan end to purple end)
-// HSV yellow=64, red is backwards at 0
-// Pin layouts on the teensy 3/3.1:
+// HSV yellow=64, red is backwards at 0 or 255
+// Pin layouts on the teensy 3.1:
 // WS2811_PORTD: 2,14,7,8,6,20,21,5
 
-#define NUM_LEDS_PER_STRIP 150
+#define NUM_LEDS_PER_STRIP 120
 #define NUM_STRIPS 8
-CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
+#define LEDCOUNT (8*120)
 
-#define LEDCOUNT NUM_STRIPS * NUM_LEDS_PER_STRIP
-// CRGB leds[LEDCOUNT+1];
+CRGB leds[LEDCOUNT+1];
+
 
 ///////////// Test sequence ////////////
 void boot_up() {
@@ -45,22 +55,22 @@ void pulse(int times) {
   for (int i=0; i < times; i++) {
     for (bright=0; bright <= 255; bright++) {
       final_hue = hue;
-      if (bright >= 191) {
+      if (bright > 190) {
         final_hue +=  delta * (bright - 191);
       }
       fill_solid( leds, LEDCOUNT, CHSV( final_hue, 255, ease8InOutCubic(bright) ) );
       FastLED.show();
-      FastLED.delay(13);
+      FastLED.delay(12);
     }
 
     for (bright=255; bright >= 0; bright--) {
       final_hue = hue;
-      if (bright >= 191) {
+      if (bright > 190) {
         final_hue +=  delta * (bright - 191);
       }
       fill_solid( leds, LEDCOUNT, CHSV( final_hue, 255, ease8InOutCubic(bright) ) );
       FastLED.show();
-      FastLED.delay(9);
+      FastLED.delay(8);
     }
 
     if (times > 1) {
@@ -130,9 +140,84 @@ void twinkle(unsigned long duration) {
     twinkle_iter();
   }
 
-  // TODO: Fade all out.
-  // Or not.. if left in, the warp mode works nicely.
 }
+
+/////////// Fire effect /////////////
+// Could also be bubble effect with different pallette
+// taken from FastLED example and modified
+// ideally should only apply to vertical strips, and should be applied
+// separately to each one
+
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 50, suggested range 20-100
+#define COOLING  60
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 65
+
+#define FIRE_NUM_LEDS 120
+
+bool gReverseDirection = false;
+void fire_iter() {
+  // Array of temperature readings at each simulation cell
+  static byte heat[FIRE_NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+  for( int i = 0; i < FIRE_NUM_LEDS; i++) {
+    heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / FIRE_NUM_LEDS) + 2));
+  }
+
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for( int k= FIRE_NUM_LEDS - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  }
+
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  if( random8() < SPARKING ) {
+    int y = random8(7);
+    heat[y] = qadd8( heat[y], random8(160,255) );
+  }
+
+  // Step 4.  Map from heat cells to LED colors
+  for( int j = 0; j < FIRE_NUM_LEDS; j++) {
+    CRGB color = HeatColor( heat[j]);
+    int pixelnumber;
+    if( gReverseDirection ) {
+      pixelnumber = (FIRE_NUM_LEDS-1) - j;
+    } else {
+      pixelnumber = j;
+    }
+    // duplicate this over all strands:
+    for (int k=0; k<8; k++) {
+      leds[k*NUM_LEDS_PER_STRIP + pixelnumber] = color;
+    }
+  }
+}
+
+void start_fire(unsigned long duration) {
+  unsigned long endpoint = millis() + duration;
+
+  for (int i=0; i < LEDCOUNT; i++) {
+    leds[i].setRGB(0,0,0);
+  }
+  FastLED.show(); // display this frame
+  FastLED.delay(10); // flush octows2811
+
+  FastLED.setBrightness(128);
+
+  while (millis() < endpoint) {
+    fire_iter();
+    FastLED.show();
+    FastLED.delay(25);
+  }
+
+  FastLED.setBrightness(255);
+}
+
+
 
 /////////////////////////////////
 
@@ -142,6 +227,8 @@ void setup() {
   random16_set_seed(analogRead(0));
 
   FastLED.addLeds<WS2811_PORTD,NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP);
+
+  FastLED.setDither(0); // I don't think it'll work right with OctoWS2811's async show
 
   // set to blank
   for (int i=0; i < LEDCOUNT; i++) {
@@ -163,8 +250,10 @@ void setup() {
 
 
 void loop() {
+  pulse(2);
+  twinkle(60000L);
+  pulse(3);
+  pulse(2);
   pulse(1);
-  pulse(7);
-  twinkle(30000L);
-  // warpspeed(false);
+  start_fire(60000L);
 }
